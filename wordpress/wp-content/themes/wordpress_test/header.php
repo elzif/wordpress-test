d2542806b6e89e5ae99c0941b5342574ef8036cb
Elliott.Holmes <elliottmholmes@gmail.com>
2018-08-24 16:36:13 +0100

    More CMS intigration

--- wordpress/wp-content/themes/wordpress_test/header.php
+++ wordpress/wp-content/themes/wordpress_test/header.php
@@ -2,40 +2,37 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
+	<meta name="author" content="">
 
-  <title>WordPress Test</title>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  <?php wp_head();?>
+	<title><?php echo get_bloginfo( 'name' ); ?></title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	<?php wp_head();?>
 </head>
 
 <body>
 
-  <div class="blog-masthead">
-    <div class="container">
-      <nav class="blog-nav">
-        <a class="blog-nav-item active" href="#">Home</a>
-        <a class="blog-nav-item" href="#">New features</a>
-        <a class="blog-nav-item" href="#">Press</a>
-        <a class="blog-nav-item" href="#">New hires</a>
-        <a class="blog-nav-item" href="#">About</a>
-      </nav>
-    </div>
-  </div>
-  
-  <div class="container">
+	<div class="blog-masthead">
+		<div class="container">
+			<nav class="blog-nav">
+				<a class="blog-nav-item active" href="<?php echo get_bloginfo( 'wpurl' );?>">Home</a>
+				<?php wp_list_pages( '&title_li=' ); ?>
+			</nav>
+		</div>
+	</div>
+	
+	<div class="container">
 
-    <div class="blog-header">
-      <h1 class="blog-title">The Bootstrap Blog</h1>
-      <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
-    </div>
+		<div class="blog-header">
+			<h1 class="blog-title"><?php echo get_bloginfo( 'name' ); ?></h1>
+			<p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+		</div>
